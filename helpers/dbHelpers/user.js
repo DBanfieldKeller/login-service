6fb06ecb30c54b88213f5f531289eee2878644e6
Goat <dpajbk@gmail.com>
2023-03-02 11:55:29 -0500

    added bcrypt, updated user helper methods

---
+++ helpers/dbHelpers/user.js
@@ -0,0 +1,52 @@
+// load the AWS SDK for node
+const AWS = require("aws-sdk");
+
+// Set the region
+AWS.config.update({ region: "us-east-2"});
+
+// Create DynamoDB document client
+const dynamoDB = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10"});
+
+const userTable = "user-login";
+
+exports.getUser = async (username) => {
+    const params = {
+        TableName: userTable,
+        Key: {
+            username: username,
+        },
+    };
+    return await dynamoDB
+        .get(params)
+        .promise()
+        .then(
+            (response) => {
+                return response.Item;
+            },
+            (error) => {
+                console.log("error fetching user", error);
+            }
+        );
+};
+
+exports.saveUser = async (user) => {
+    const params = {
+        TableName: userTable,
+        Key: {
+            username: user.username,
+        },
+        Item: user
+    };
+
+    return await dynamoDB
+        .put(params)
+        .promise()
+        .then(
+            (response) => {
+                return true;
+            },
+            (error) => {
+                console.log("Error saving user", error);
+            }
+        )
+};
